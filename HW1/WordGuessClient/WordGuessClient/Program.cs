2c4138e09cd8699b4f50837f5451da887d4dbbe2
Jason Bingham <jbingham408@gmail.com>
2017-09-07 17:56:19 -0600

    Created Message(added methods to decode and encode messages, Receiver, and Sender classes

--- HW1/WordGuessClient/WordGuessClient/Program.cs
+++ HW1/WordGuessClient/WordGuessClient/Program.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using log4net;
+using log4net.Config;
+
 
 namespace WordGuessClient
 {
-    static class Program
+    class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Program));
+
         [STAThread]
         static void Main()
         {
+            XmlConfigurator.Configure();
+            logger.Info("Starting Client");
+            logger.Debug("test");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new WordGuessClient());
